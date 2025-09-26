373987ee5f4724775b44de9c5a516b5befbf157c
shinnnr <lunaaraymond@gmail.com>
2025-09-27 03:44:06 +0800

    Smart Message Timestamp Implementation 2

--- app.py
+++ app.py
@@ -161,3 +161,6 @@ with app.app_context():
         else:
             # Previous day: show time and date
             return manila_dt.strftime('%I:%M %p<br><small class="text-muted">%b %d, %Y</small>')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
